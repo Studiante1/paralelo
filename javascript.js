cfd505248beb8b9a05c1909895bed52910618966
Estudiante1 <estudiante1@protonmail.com>
2018-06-25 15:24:38 +0200

    update 25618

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// var vuemirror = new Vue({
+//   el: '#vuemirror',
+//   data: {
+//     message: 'BOSTON DYNAMICS'
+//   },
+//   methods: {
+//     reverseMessage: function () {
+//       this.message = this.message.split('').reverse().join('')
+//     }
+//   }
+// })
